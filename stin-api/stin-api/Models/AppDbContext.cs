17539c5dc37bc804214542b10663f3d3fd33c48a
Petr_Mikenda <petr.mikenda@tul.cz>
2024-05-08 23:08:12 +0200

    initial

---
+++ stin-api/stin-api/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace stin_api.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; } = null!;
+    }
+}
