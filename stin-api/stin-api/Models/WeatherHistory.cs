f8fea31b294f576a2b802d924ceb9e58b78dc317
Petr_Mikenda <petr.mikenda@tul.cz>
2024-05-21 21:04:32 +0200

    weather history endpoint
    added endpoint for 5 days weather history

---
+++ stin-api/stin-api/Models/WeatherHistory.cs
@@ -0,0 +1,31 @@
+﻿namespace stin_api.Models
+{
+    public class List
+    {
+        public int dt { get; set; }
+        public MainHistory main { get; set; }
+        public Wind wind { get; set; }
+        public Clouds clouds { get; set; }
+        public List<Weather> weather { get; set; }
+    }
+
+    public class MainHistory
+    {
+        public double temp { get; set; }
+        public double feels_like { get; set; }
+        public int pressure { get; set; }
+        public int humidity { get; set; }
+        public double temp_min { get; set; }
+        public double temp_max { get; set; }
+    }
+
+    public class WeatherHistory
+    {
+        public string message { get; set; }
+        public string cod { get; set; }
+        public int city_id { get; set; }
+        public double calctime { get; set; }
+        public int cnt { get; set; }
+        public List<List> list { get; set; }
+    }
+}
